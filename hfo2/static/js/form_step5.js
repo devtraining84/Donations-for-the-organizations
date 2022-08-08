a219564591000a14d65cd4f0109dc8a1fabc147e
devtraining84 <dev.training.84@gmail.com>
2022-08-08 23:08:49 +0200

    added js for step 5 of form

---
+++ hfo2/static/js/form_step5.js
@@ -0,0 +1,54 @@
+
+document.addEventListener("DOMContentLoaded", function() {
+    console.log("test of step 5");
+    var button4 = document.getElementById("button4");
+    var bags = document.getElementById("bags");
+    var address = document.getElementById("address");
+    var city = document.getElementById("city");
+    var postcode = document.getElementById("postcode");
+    var phone = document.getElementById("phone");
+    var date = document.getElementById("date");
+    var time = document.getElementById("time");
+    var more_info = document.getElementById("more_info");
+  
+
+    
+    
+    button4.addEventListener('click', function (e) {
+        console.log('click on button4');
+    
+        var data = $('form').serializeArray().reduce(function(obj, item) {
+            
+            obj[item.name] = item.value;
+            console.log(obj);
+            bags.innerText = (obj.bags + " workow z darami");
+            address.innerText = obj.address;
+            city.innerText = obj.city;
+            postcode.innerText = obj.postcode;
+            phone.innerText = obj.phone;
+            date.innerText = obj.date;
+            time.innerText = obj.time;
+            more_info.innerText = obj.more_info;
+
+
+            
+            return obj;
+    
+        }, {});
+        
+    
+    
+    });
+
+    
+
+
+
+
+
+
+    });
+    
+    
+    
+    
