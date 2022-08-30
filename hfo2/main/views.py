8c736185ad2ce192079727d2f5dd44134b14c9f0
devtraining84 <dev.training.84@gmail.com>
2022-08-30 19:01:52 +0200

    work with form

--- hfo2/main/views.py
+++ hfo2/main/views.py
@@ -8,7 +8,7 @@ from django.contrib.auth.mixins import LoginRequiredMixin
 from django.contrib.auth import authenticate, login, logout
 from django.views import View
 from django.views.generic import TemplateView
-from main.models import CategoryModel, InstitutionModel, DonationModel, TestModel2
+from main.models import CategoryModel, InstitutionModel, DonationModel, TestModel, TestModel2
 from main.forms import LoginForm, UserCreateForm, UserEditForm
 
 # Create your views here.
@@ -111,37 +111,39 @@ class AddDonationView(LoginRequiredMixin, View):
     # pass
     def get(self, request):
         categories = CategoryModel.objects.all()
-        organizations = InstitutionModel.objects.all()
+       
         ctx = {
-            'categories': categories,
-            'organizations': organizations,
-            }
-        return render(request, 'sub_form.html', ctx)
+             'categories': categories,
+             }
+         
+
+        return render(request,'sub_form.html',ctx)
     
     def post(self, request):
-        
+            
        
-        categories_list = request.POST.getlist("categories")
-        categories = CategoryModel.objects.filter(pk__in = categories_list)
-        # pk or name, in depend on value in form
+        # categories_list = request.POST.getlist("categories")
+        # categories = CategoryModel.objects.filter(pk__in = categories_list)
+        # # pk or name, in depend on value in form
         
         
 
-        data = TestModel2()
+        data = TestModel()
         data.quantity = request.POST.get("bags")
-        data.address = request.POST.get("address")
-        data.city = request.POST.get("city")
-        data.zip_code = request.POST.get("postcode")
-        data.phone_number = request.POST.get("phone")
-        data.pick_up_date = request.POST.get("date")            
-        data.pick_up_time = request.POST.get("time")
-        #worktest
-        data.pick_up_comment = request.POST.get("organization")
-        
-        #data.user = request.user.id
-        
-        
         data.save()
+        # data.address = request.POST.get("address")
+        # data.city = request.POST.get("city")
+        # data.zip_code = request.POST.get("postcode")
+        # data.phone_number = request.POST.get("phone")
+        # data.pick_up_date = request.POST.get("date")            
+        # data.pick_up_time = request.POST.get("time")
+        # #worktest
+        pick_up_comment = request.POST.get("bugs")
+        
+        # #data.user = request.user.id
+        
+        
+        # data.save()
         return redirect('/donationconfirm/')
 
        
---
+++ hfo2/static/js/sending.js
@@ -0,0 +1,9 @@
+document.addEventListener("DOMContentLoaded", function() {
+
+var submit = document.getElementById("submit");
+submit.click(console.log("submit click"))
+
+
+
+
+});    
